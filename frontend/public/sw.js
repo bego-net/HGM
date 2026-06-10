d99c5cb3c7799386177d4b1287d2ff2d4598fe7f
Begonet Debebe <begonetdebebe@gmail.com>
2026-06-10 13:08:00 +0300

    feat(pwa): add simple service worker to cache scanner assets

---
+++ frontend/public/sw.js
@@ -0,0 +1,30 @@
+const CACHE_NAME = 'eids-scanner-v1';
+const ASSETS_TO_CACHE = [
+  '/',
+  '/scanner',
+  '/manifest.json'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS_TO_CACHE))
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(
+        keys.map((k) => {
+          if (k !== CACHE_NAME) return caches.delete(k);
+        })
+      )
+    )
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((res) => res || fetch(event.request))
+  );
+});
